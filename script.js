84f7657cea2040a01f0e820706ce0719fe285bf4
AngadJ <angad.joneja@gmail.com>
2022-11-14 10:28:56 -0500

    school copy

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getData = async (url, params) => {
     console.log(error);
   }
 };
- 
+
 const getMovies1 = async () => {
   let datatag = document.getElementById("datatag");
   datatag.innerHTML = "";
@@ -22,7 +22,7 @@ const getMovies1 = async () => {
   if (movieData.data.results.length < 1) {
     return;
   }
- 
+  
     let movie = movieData.data.results[0];
     console.log("movie: " + movie)
     const extraData = await getData(`https://api.themoviedb.org/3/movie/${movie.id}`, {
@@ -31,17 +31,17 @@ const getMovies1 = async () => {
         append_to_response: "videos",
       }
     });
-
+    
+    const img = document.createElement('img');
     const iframe = document.createElement('iframe');
     const p = document.createElement('p');
     p.setAttribute('id','para1')
-    const img = document.createElement('img');
     const h1 =document.createElement("h1")
     const trailer = extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key;
  
-    p.innerHTML = `${movie.title} -- Release Date: ${movie.release_date} -- Rating: ${extraData.data.vote_average}/10 Stars -- Length of Film: ${extraData.data.runtime} minutes -- Popularity: ${movie.popularity} -- 
-    Box Office: ${extraData.data.revenue} USD -- Budget: ${extraData.data.budget} USD -- Overview: ${movie.overview}`
     img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    p.innerHTML = `<p> Release Date: ${movie.release_date} <p> Rating: ${extraData.data.vote_average} Stars <p> Length of Film: ${extraData.data.runtime} minutes <p> Popularity: ${movie.popularity} <p>
+    Box Office: ${extraData.data.revenue} USD <p> Budget: ${extraData.data.budget} USD <p> Vote Count: ${extraData.data.vote_count} people <p> Overview: ${movie.overview} `
     h1.innerHTML = `${extraData.data.original_title}`
     iframe.src = `https://www.youtube.com/embed/${trailer}`
 
